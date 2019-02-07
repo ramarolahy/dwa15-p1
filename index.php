3b6cb376fce610cfe84905b287e35613bda876aa
Mbinintsoa Ramarolahy <mbinintsoa.ramarolahy@gmail.com>
2019-02-07 13:21:57 -0700

    init commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <title>Project 1</title>
+</head>
+<body>
+    <div class="container">
+        <div class="card" style="width: 50rem;">
+            <div class="card-body">
+                <h1 class="card-title text-center">Mbinintsoa "Ram" Ramarolahy</h1>
+            </div>
+            <img src="./img/me.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">About me</h5>
+                <p class="card-text">Hello everyone!
+                    My name is Mbinintsoa Ramarolahy, you can call me <b>Ram</b> for short. I live in Colorado Springs, CO.
+                    My background is in HR, Retail Management, and Banking but I am working on switching to UX Design and Software Dev since this has always been my dream.
+                    I received my BS Degree in Engineering Management from the US Air Force Academy and I am now pursuing a Web Technologies Certificate from HES (Hopefully continuing to a Masters in Digital Media Design)
+
+                    I chose to register for this course due to PHP's dominance in the web industry. I just took intro to JS and Fund of Web Dev last semester which I enjoyed. I am excited to learn PHP with everyone!
+
+                    Ram.</p>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Random Quote</h5>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
